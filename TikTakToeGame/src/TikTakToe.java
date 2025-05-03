bbebf9efdc48709a8457632a2b8a676390b46d84
nikolay-rangelov <nikolai.rangeloff@gmail.com>
2025-05-03 19:14:52 +0300

    Tik Tak Toe game finished

--- TikTakToeGame/src/TikTakToe.java
+++ TikTakToeGame/src/TikTakToe.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import static java.lang.System.in;
@@ -6,6 +8,7 @@ import static java.lang.System.in;
 public class TikTakToe {
 
     public static char winner = ' ';
+    public static String playerName = null;
 
     public static void printBoard(char[][] board) {
         System.out.printf(" _ _ _ %n");
@@ -99,19 +102,43 @@ public class TikTakToe {
         return status;
     }
 
-    public static void handleUserInput (int player, char board[][], char symbol) {
+    public static boolean isInt(String num){
+        if (num == null) {
+            return false;
+        }
+        try {
+            int i = Integer.parseInt(num);
+        } catch (NumberFormatException err) {
+            return false;
+        }
+        return true;
+    }
+
+    public static int handleUserInputInt() {
+        Scanner sc = new Scanner(in);
+        int num;
+        String input = sc.nextLine();
+        while (!isInt(input)) {
+            System.out.printf("Not a valid number, please enter again: ");
+            input = sc.nextLine();
+        }
+        num = Integer.parseInt(input);
+        return num;
+    }
+
+    public static void handleUserInput (char board[][], char symbol) {
         // It breaks when user inputs nothing, should be fixed.
         Scanner sc = new Scanner(in);
-        System.out.printf("Player %d {%c} turn, choose position: ", player, symbol);
-        int choice = Integer.parseInt(sc.nextLine());
+        System.out.printf("Player: %s's {%c} turn, choose position: ", playerName, symbol);
+        int choice = handleUserInputInt();
         while (choice < 1 || choice > 9) {
             System.out.printf("Error: No such position. Insert position again: ");
-            choice = Integer.parseInt(sc.nextLine());
+            choice = handleUserInputInt();
         }
         int status = insertUserChoiceInBoard(choice, board, symbol);
         while (status == 1) {
             System.out.printf("Error: Position is already filled. Insert position again: ");
-            choice = Integer.parseInt(sc.nextLine());
+            choice = handleUserInputInt();
             status = insertUserChoiceInBoard(choice, board, symbol);
         }
     }
@@ -219,33 +246,348 @@ public class TikTakToe {
         return win;
     }
 
+    public static int[] checkRowBeforeWinning(int r, char[][] board, char symbol){
+        int timesSeen = 0;
+        int emptyCell = 0;
+        int[] coordinates = {0, -1, -1};
+        for (int c = 0; c < 3; c++) {
+            if (symbol == board[r][c]) {
+                timesSeen++;
+            }
+            if (board[r][c] != 'X' && board[r][c] != 'O') {
+                emptyCell++;
+                coordinates[1] = r;
+                coordinates[2] = c;
+            }
+        }
+
+        if (timesSeen == 2 && emptyCell == 1) {
+            coordinates[0] = 1;
+        }
+        return coordinates;
+    }
+
+    public static int[] checkColumnBeforeWinning(int c, char[][] board, char symbol){
+        int timesSeen = 0;
+        int emptyCell = 0;
+        int[] coordinates = {0, -1, -1};
+        for (int r = 0; r < 3; r++) {
+            if (symbol == board[r][c]) {
+                timesSeen++;
+            }
+            if (board[r][c] != 'X' && board[r][c] != 'O') {
+                emptyCell++;
+                coordinates[1] = r;
+                coordinates[2] = c;
+            }
+        }
+
+        if (timesSeen == 2 && emptyCell == 1) {
+            coordinates[0] = 1;
+        }
+        return coordinates;
+    }
+
+    public static int[] checkDiagonalsBeforeWinning(char[][] board, char symbol){
+        int timesSeen = 0;
+        int emptyCell = 0;
+        int[] coordinates = {0, -1, -1};
+        int r = 0;
+        int c = 0;
+        for (int i = 0; i < 3; i++) {
+            if (symbol == board[r][c]) {
+                timesSeen++;
+            }
+            if (board[r][c] != 'X' && board[r][c] != 'O') {
+                emptyCell++;
+                coordinates[1] = r;
+                coordinates[2] = c;
+            }
+            r++;
+            c++;
+        }
+        if (timesSeen == 2 && emptyCell == 1) {
+            coordinates[0] = 1;
+        }
+
+        if(coordinates[0] == 0) {
+            timesSeen = 0;
+            emptyCell = 0;
+            r = 2;
+            c = 0;
+            for (int i = 0; i < 3; i++) {
+                if (symbol == board[r][c]) {
+                    timesSeen++;
+                }
+                if (board[r][c] != 'X' && board[r][c] != 'O') {
+                    emptyCell++;
+                    coordinates[1] = r;
+                    coordinates[2] = c;
+                }
+                r--;
+                c++;
+            }
+            if (timesSeen == 2 && emptyCell == 1) {
+                coordinates[0] = 1;
+            }
+        }
+        return coordinates;
+    }
+
+
+    public static int[] checkBeforeWinning (char[][] board, char symbol){
+        int result[] = {0, -1, -1};
+        //check rows before winning
+        for (int r = 0; r < 3; r++) {
+            result = checkRowBeforeWinning(r, board, symbol);
+            if(result[0] == 1) {
+                break;
+            }
+        }
+         //check columns before winning
+        if (result[0] == 0) {
+            for (int c = 0; c < 3; c++) {
+                result = checkColumnBeforeWinning(c, board, symbol);
+                if(result[0] == 1) {
+                    break;
+                }
+            }
+        }
+
+        // check diagonals before winning
+        if (result[0] == 0) {
+            result = checkDiagonalsBeforeWinning(board, 'X');
+        }
+
+        return result;
+    }
+
+    public static void calculateRowScore(int r, char[][] board, int[][] boardScore, char symbol) {
+        int timesSeen = 0;
+        int emptyCell = 0;
+        for (int c = 0; c < 3; c++) {
+            if (symbol == board[r][c]) {
+                timesSeen++;
+            }
+            if (board[r][c] != 'X' && board[r][c] != 'O') {
+                emptyCell++;
+            }
+        }
+        for (int c = 0; c < 3; c++) {
+            if (board[r][c] != 'X' && board[r][c] != 'O') {
+                boardScore[r][c] += emptyCell - timesSeen;
+            }
+        }
+    }
+
+    public static void calculateColumnScore(int c, char[][] board, int[][] boardScore, char symbol) {
+        int timesSeen = 0;
+        int emptyCell = 0;
+        for (int r = 0; r < 3; r++) {
+            if (symbol == board[r][c]) {
+                timesSeen++;
+            }
+            if (board[r][c] != 'X' && board[r][c] != 'O') {
+                emptyCell++;
+            }
+        }
+        for (int r = 0; r < 3; r++) {
+            if (board[r][c] != 'X' && board[r][c] != 'O') {
+                boardScore[r][c] += emptyCell - timesSeen;
+            }
+        }
+    }
+
+    public static void calculateDiagonalsScore(char[][] board, int[][] boardScore, char symbol) {
+        int timesSeen = 0;
+        int emptyCell = 0;
+        int r = 0;
+        int c = 0;
+        for (int i = 0; i < 3; i++) {
+            if (symbol == board[r][c]) {
+                timesSeen++;
+            }
+            if (board[r][c] != 'X' && board[r][c] != 'O') {
+                emptyCell++;
+            }
+            r++;
+            c++;
+        }
+
+        r = 0;
+        c = 0;
+        for (int i = 0; i < 3; i++) {
+            if (board[r][c] != 'X' && board[r][c] != 'O') {
+                boardScore[r][c] += emptyCell + timesSeen;
+            }
+            r++;
+            c++;
+        }
+
+        r = 2;
+        c = 0;
+        timesSeen = 0;
+        emptyCell = 0;
+        for (int i = 0; i < 3; i++) {
+            if (symbol == board[r][c]) {
+                timesSeen++;
+            }
+            if (board[r][c] != 'X' && board[r][c] != 'O') {
+                emptyCell++;
+            }
+            r--;
+            c++;
+        }
+
+        r = 2;
+        c = 0;
+        for (int i = 0; i < 3; i++) {
+            if (board[r][c] != 'X' && board[r][c] != 'O') {
+                boardScore[r][c] += emptyCell + timesSeen;
+            }
+            r--;
+            c++;
+        }
+    }
+
+    public static void calculateBoardScore(char[][] board, int[][] boardScore, char symbol) {
+        // calculate row score
+        for (int r = 0; r < 3; r++) {
+            calculateRowScore(r, board, boardScore, symbol);
+        }
+        // calculate column score
+        for (int c = 0; c < 3; c++) {
+            calculateColumnScore(c, board, boardScore, symbol);
+        }
+        calculateDiagonalsScore(board, boardScore, symbol);
+    }
+
+    public static void printBoardScore(int scoreBoard[][])
+    {
+        for (int r = 0; r < 3; r++) {
+            System.out.print("[");
+            for (int c = 0; c < 3; c++) {
+                if (c < 2) {
+                    System.out.printf("%d, ", scoreBoard[r][c]);
+                } else {
+                    System.out.printf("%d", scoreBoard[r][c]);
+                }
+            }
+            System.out.printf("]%n");
+        }
+    }
+
+    public static int randomNumberInRange(int min, int max) {
+        int randomNum = min + (int)(Math.random() * ((max - min) + 1));
+        return randomNum;
+    }
+
+    public static int[] findBestMoveBasedOnScoreBoard(int scoreBoard[][]) {
+        int[] nextMove = {0, 0};
+        int max = Integer.MIN_VALUE;
+        List<int[]> foundMoves = new ArrayList<int[]>(); // Create an ArrayList object
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (max < scoreBoard[r][c]) {
+                    max = scoreBoard[r][c];
+                }
+            }
+        }
+
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (max == scoreBoard[r][c]) {
+                    int[] point = {r, c};
+                    foundMoves.add(point);
+                }
+            }
+        }
+
+        if (foundMoves.size() == 1) {
+            nextMove = foundMoves.get(0);
+        } else {
+            int size = foundMoves.size();
+            nextMove = foundMoves.get(randomNumberInRange(0, size-1));
+        }
+        return nextMove;
+    }
+
+
+    public static void gameEngine(char[][] board) {
+         //block the other player before winning
+        boolean decisionTaken = false;
+        int[] result = checkBeforeWinning(board, 'X');
+
+        if (result[0] == 1) {
+            char cellNum = board[result[1]][result[2]];
+            board[result[1]][result[2]] = 'O';
+            System.out.printf("The computer has made the following move: %c.%n", cellNum);
+            decisionTaken = true;
+        }
+
+        // check if the computer is about to win
+        if (!decisionTaken) {
+            result = checkBeforeWinning(board, 'O');
+            if (result[0] == 1) {
+                char cellNum = board[result[1]][result[2]];
+                board[result[1]][result[2]] = 'O';
+                System.out.printf("The computer has made the following move: %c.%n", cellNum);
+                decisionTaken = true;
+            }
+        }
+
+        // calculate score for each box if decision is still not taken
+        if (!decisionTaken) {
+            int[][] boardScore = {{ 0, 0, 0},
+                                  { 0, 0, 0},
+                                  { 0, 0, 0}};
+            calculateBoardScore(board, boardScore, 'O');
+            result = findBestMoveBasedOnScoreBoard(boardScore);
+            char cellNum = board[result[0]][result[1]];
+            board[result[0]][result[1]] = 'O';
+            System.out.printf("The computer has made the following move: %c.%n",cellNum);
+            //printBoardScore(boardScore);
+        }
+
+    }
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(in);
         char[][] board = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
         boolean gameWon = false;
+        System.out.printf("Please, enter your name to start the game: ");
+        playerName = sc.nextLine();
+        while (playerName == "") {
+            System.out.printf("No input, please, enter your name to start the game: ");
+            playerName = sc.nextLine();
+        }
+        System.out.printf("Hello, %s! The game starts!%n", playerName);
         System.out.printf("---Game started---%n");
         printBoard(board);
         int player = 1;
         for (int i = 0; i < 9; i++) {
             if (player == 1) {
-                handleUserInput(player, board, 'X');
+                handleUserInput(board, 'X');
                 player = 2;
             } else {
-                handleUserInput(player, board, 'O');
+                gameEngine(board);
                 player = 1;
             }
             printBoard(board);
             gameWon = checkIfWon(board);
             if (gameWon) {
-                if (winner == 'X') {
-                    System.out.printf("Congratulations, Player 1 {%c} won the game!%n", 'X');
-                } else {
-                    System.out.printf("Congratulations, Player 2 {%c} won the game!%n", 'O');
-                }
                 break;
-            } else {
-                System.out.printf("No one won the game.%n");
             }
         }
-        System.out.printf("---Game finished---");
+        if (gameWon) {
+            if (winner == 'X') {
+                System.out.printf("Congratulations, %s {%c} won the game!%n", playerName, 'X');
+            } else {
+                System.out.printf("You lost, %s {%c}. The Computer {%c} won the game!%n", playerName, 'X', 'O');
+            }
+        } else {
+            System.out.printf("No one won the game.%n");
+        }
+        System.out.printf("---Game finished, thank you for playing!---");
     }
 }
